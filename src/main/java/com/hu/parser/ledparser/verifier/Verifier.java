16908e2ca1508ac54644c4979803a44c1359e31b
whoiszeronull <shunning_hu@163.com>
2019-01-18 20:58:30 +0800

    try to read xml config

--- src/main/java/com/hu/parser/ledparser/verifier/Verifier.java
+++ src/main/java/com/hu/parser/ledparser/verifier/Verifier.java
@@ -1,11 +1,14 @@
 package com.hu.parser.ledparser.verifier;
 
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 public interface Verifier {
 	
 	/**
 	 * To verify the text content having necessary information or not.
 	 * @param content
 	 * @return
+	 * @throws  
 	 */
 	public boolean verifiy(String content);
 
--- src/main/java/com/hu/parser/ledparser/verifier/impl/HtmlTableSpecsVerifier.java
+++ src/main/java/com/hu/parser/ledparser/verifier/impl/HtmlTableSpecsVerifier.java
@@ -1,9 +1,17 @@
 package com.hu.parser.ledparser.verifier.impl;
 
+import java.util.List;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 import com.hu.parser.ledparser.verifier.Verifier;
 
 /**
- * HtmlTableSpecVerifier is to determin whether the passed string content having specification infomations in the html table format.
+ * HtmlTableSpecVerifier is to determin whether the passed string content having
+ * specification infomations in the html table format.
+ * 
  * @author shunn
  *
  */
@@ -11,9 +19,20 @@ public class HtmlTableSpecsVerifier implements Verifier {
 
 	@Override
 	public boolean verifiy(String content) {
-		
-		
+
 		return false;
+	}
+
+	public List<Verifier> getVerifiers() {
+		Configurations configs = new Configurations();
+		XMLConfiguration config;
+		try {
+			config = configs.xml("./config/parser-config.xml");
+			return config.getList(Verifier.class, "verifiers.verifier");
+			
+		} catch (ConfigurationException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
