81a2b845d2a06ed4ca72cdf56c3df799164eda70
whoiszeronull <shunning_hu@163.com>
2019-01-19 02:16:00 +0800

    processing the parser config

--- src/main/java/com/hu/parser/ledparser/verifier/impl/HtmlTableSpecsVerifier.java
+++ src/main/java/com/hu/parser/ledparser/verifier/impl/HtmlTableSpecsVerifier.java
@@ -1,11 +1,5 @@
 package com.hu.parser.ledparser.verifier.impl;
 
-import java.util.List;
-
-import org.apache.commons.configuration2.XMLConfiguration;
-import org.apache.commons.configuration2.builder.fluent.Configurations;
-import org.apache.commons.configuration2.ex.ConfigurationException;
-
 import com.hu.parser.ledparser.verifier.Verifier;
 
 /**
@@ -19,20 +13,7 @@ public class HtmlTableSpecsVerifier implements Verifier {
 
 	@Override
 	public boolean verifiy(String content) {
-
 		return false;
 	}
-
-	public List<String> getVerifiers() {
-		Configurations configs = new Configurations();
-		XMLConfiguration config;
-		try {
-			config = configs.xml("./config/parser-config.xml");
-			return config.getList(String.class, "verifiers.verifier");
-			
-		} catch (ConfigurationException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
+	
 }
